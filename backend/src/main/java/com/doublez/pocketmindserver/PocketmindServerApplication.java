971bf8a158a531aa2bb66e32c48df14f3db0e868
doublez <1624475081@qq.com>
2025-11-07 01:28:42 +0800

    feat: 后端工程初始化

---
+++ backend/src/main/java/com/doublez/pocketmindserver/PocketmindServerApplication.java
@@ -0,0 +1,13 @@
+package com.doublez.pocketmindserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketmindServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketmindServerApplication.class, args);
+    }
+
+}
